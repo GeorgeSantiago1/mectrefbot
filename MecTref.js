595f3c244fe9582bee5bf5af0bb3d8ec51a18be8
GeorgeSantiago1 <42331218+GeorgeSantiago1@users.noreply.github.com>
2018-08-17 13:56:07 -0300

    Add files via upload

---
+++ MecTref.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+ const client = new Discord.Client();
+ client.login("NDc4MzkzNzc0MjU5NTY4NjQx.DlaTCw.rpkLwjXcyDYH2wf_aj6Wq-5ZI_o");
+ client.on('ready', () =>{     
+   console.log('[Debora] Online - MECTREFE')  });
+ client.on('guildMemberAdd', member =>{
+    member.send("'Oiii amorzinho, tudo bem? Desculpa te mandar essa mensagem assim que você entrou, é que eu gostaria de convidar você para um servidor, lá você pode conversar, conhecer pessoas novas, fazer novas amizades, achar pessoas pra você jogar, e em breve também teremos eventos, sorteios e muito mais. Aparece lá, para a gente jogar uns games qualquer hora, prometo fazer valer a pena a tua entrada, te espero lá  :heart: https://discord.gg/rbTAXwF https://gph.is/2JUamwq'")
+});
+client.login("NDc5ODM4NzQ4OTU2Mjk1MTcw.DlfH3g.aWV2F804HBkih871v0ioqzWO1jM");
+ client.on('ready', () =>{     
+   console.log('[Isis] Online | MecTrefe') });
+ client.on('guildMemberAdd', member =>{
+    member.send("'Oiii amorzinho, tudo bem? Desculpa te mandar essa mensagem assim que você entrou, é que eu gostaria de convidar você para um servidor, lá você pode conversar, conhecer pessoas novas, fazer novas amizades, achar pessoas pra você jogar, e em breve também teremos eventos, sorteios e muito mais. Aparece lá, para a gente jogar uns games qualquer hora, prometo fazer valer a pena a tua entrada, te espero lá  :heart: https://discord.gg/rbTAXwF https://gph.is/2JUamwq'") 
+ });
